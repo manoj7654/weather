feb5d60c61ce388cd8a699eb4e9c7c9b69dfbbca
Manoj Kumar <manojsfstm5@gmail.com>
2023-06-01 10:17:23 +0530

    weather app backend

--- backend/config/db.js
+++
@@ -1,7 +0,0 @@
-// importing mongoose for making connection
-const mongoose=require("mongoose");
-require("dotenv").config();
-
-const connection=mongoose.connect(process.env.mongoUrl);
-
-module.exports={connection}
